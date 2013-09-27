c6507ee381fff515350ade22a832d8a66d886e72
baolongnt <baolongnt@0fa9042c-1658-466d-a3ad-57b394c83ab8>
2013-09-27 16:53:44 +0000

    . Updated build.xml to latest SDK . Updated release.sh. Note: the new build.xml now automates signing and zipalign. Make sure to update key.store and key.alias in ant.properties

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@
 
 rm -rf bin
 ant clean release
-cp bin/EmailPopup-unsigned.apk bin/EmailPopup-signed.apk
-jarsigner -verbose -keystore ../../Documents/projects/Email\ Popup/misc/blntsoft-android.keystore bin/EmailPopup-signed.apk blntsoft-android
-zipalign -v 4 bin/EmailPopup-signed.apk bin/EmailPopup-zipaligned.apk
+cp bin/EmailPopup-release-unsigned.apk bin/EmailPopup-release-signed.apk
+jarsigner -verbose -keystore ../../Documents/projects/Email\ Popup/misc/blntsoft-android.keystore bin/EmailPopup-release-signed.apk blntsoft-android
+zipalign -v 4 bin/EmailPopup-release-signed.apk bin/EmailPopup-release-zipaligned.apk
